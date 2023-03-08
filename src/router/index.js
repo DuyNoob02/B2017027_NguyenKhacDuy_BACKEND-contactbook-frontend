7b6967e4011f00920ea14e4886a5eb6e3daaae7b
DuyNoob02 <nguyenkhachuy25122002@gmail.com>
2023-03-08 19:41:03 +0700

    Tao trang lien he

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
+
 const routes = [
     {
         path: "/",
@@ -7,10 +8,23 @@ const routes = [
         component: ContactBook,
     },
     {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true,
+    },
+    {
+        path: "/contact/add",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+        props: true,
+    },
+
+    {
         path: "/:pathMatch(.*)*",
         name: "notfound",
         component: () => import("@/views/NotFound.vue"),
-    }
+    },
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
